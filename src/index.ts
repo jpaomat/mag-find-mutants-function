da4e9b0ba0cc1253e3d75fc599c69ece0a83c4d5
Jenny Matamoros <jpaomat@gmail.com>
2021-12-25 19:57:55 -0500

    Fixing errors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Context, Handler } from "aws-lambda";
-import { HttpStatus } from "./config/httpStatus";
+import { Context, Handler } from 'aws-lambda';
+import { HttpStatus } from './config/httpStatus';
 
-interface ReqEvent {
+interface IReqEvent {
     key1: string;
     key2: string;
     key3: string;
-};
-
-interface Res {
-    statusCode: number;
-    body: string;
 }
 
-export const handler: Handler<ReqEvent, Res> = async (event: ReqEvent, context: Context): Promise<Res> => {
+interface IRes {
+    body: string;
+    statusCode: number;
+}
+
+export const handler: Handler<IReqEvent, IRes> = async (event: IReqEvent, context: Context): Promise<IRes> => {
     const response = {
         statusCode: HttpStatus.OK,
         body: 'Hola'
