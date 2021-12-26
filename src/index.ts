a24c291d0bde93b72a31ce8a6e841b3a28ea8416
Jenny Matamoros <jpaomat@gmail.com>
2021-12-26 03:05:05 -0500

    Testing connection with DB DNA_VERIFICATION_MUTANTS

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import { IReqEvent } from './models/IReqEvent';
 import { IRes } from './models/IResponse';
 
 export const handler: Handler<IReqEvent, IRes> = async (event: IReqEvent, context: Context): Promise<IRes> => {
+    console.log('Index 8 - Input to mag-find-mutants-function lambda: ', event);
     let dnaData = event.dna;
     const insertDnaDB = new InsertVerificationDNA();
-    const dnaSegment = JSON.stringify(dnaData);
+    const dnaSegment = `'${dnaData}'`;
     const resultInsert = await insertDnaDB.insertDnaResult(dnaSegment, 1);
     console.log('resultInsert', resultInsert);
     return {
