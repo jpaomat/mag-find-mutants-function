f98cca2b39f2a70f45f88b1a7a705a1509b92078
Jenny Matamoros <jpaomat@gmail.com>
2021-12-26 02:48:02 -0500

    Testing connection with DB DNA_VERIFICATION_MUTANTS

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import { IRes } from './models/IResponse';
 
 export const handler: Handler<IReqEvent, IRes> = async (event: IReqEvent, context: Context): Promise<IRes> => {
     const insertDnaDB = new InsertVerificationDNA();
-    const resultInsert = await insertDnaDB.insertDnaResult(['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG'].toString(), 1);
+    const dnaSegment = `['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG']`;
+    const resultInsert = await insertDnaDB.insertDnaResult(dnaSegment, 1);
     console.log('resultInsert', resultInsert);
     return {
         body: 'Hola',
