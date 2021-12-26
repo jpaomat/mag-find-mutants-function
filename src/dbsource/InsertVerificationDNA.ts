438b7067a74201568919bc955d9ffb138e9e3553
Jenny Matamoros <jpaomat@gmail.com>
2021-12-26 02:27:34 -0500

    Testing connection with DB DNA_VERIFICATION_MUTANTS

---
+++ src/dbsource/InsertVerificationDNA.ts
@@ -0,0 +1,29 @@
+import ConnectionDb from '../config/ConnectionDb';
+
+export default class InsertVerificationDNA {
+    public async insertDnaResult(dnaSegment: string, isMutant: number) {
+        const clsConnectionDB = new ConnectionDb('/rds_db/mysql');
+        const table = 'gmutants_general.DNA_VERIFICATION_MUTANTS';
+        await clsConnectionDB.getCredentialDb();
+        const query = `INSERT INTO ${table} (DNA, MUTANT) VALUES (${dnaSegment}, ${isMutant});`;
+        const connection = await clsConnectionDB.getConnectionSingleton();
+        return this.callQuery(connection, query);
+
+    }
+
+    private callQuery(connection: any, queryCustom: string) {
+        return new Promise((resolve, reject) => {
+            connection.query(queryCustom, (err: any, result: any) => {
+                console.log('callQuery 17', queryCustom);
+                console.log('callQuery 18', result);
+                if (err) {
+                    console.log('Error to execute query', err);
+                    return reject(err);
+                } else {
+                    return resolve(result);
+                }
+            });
+            connection.release();
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Context, Handler } from 'aws-lambda';
-import ConnectionDb from './config/ConnectionDb';
 import { HttpStatus } from './config/httpStatus';
+import InsertVerificationDNA from './dbsource/InsertVerificationDNA';
 import { IReqEvent } from './models/IReqEvent';
 import { IRes } from './models/IResponse';
 
 export const handler: Handler<IReqEvent, IRes> = async (event: IReqEvent, context: Context): Promise<IRes> => {
-    const clsConnectionDB = new ConnectionDb('/rds_db/mysql');
-    await clsConnectionDB.getCredentialDb();
+    const insertDnaDB = new InsertVerificationDNA();
+    insertDnaDB.insertDnaResult(['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG'].toString(), 1);
     return {
         body: 'Hola',
         statusCode: HttpStatus.OK,
